d5f95b071a48d84d8c56c1f400bd845486001bb7
Marianne Cowherd <mariannecowherd@gmail.com>
2021-09-13 14:05:56 -0700

    add reprojected max swe

--- reproject.sh
+++ reproject.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 targetfile='c:/users/marianne/documents/pe_snow_fires/pedata/NCALDAS_NOAH0125_D.A19790103.002.nc4.SUB.tif'
-infile='swe_test.tif'
-outfile='output_test.tif'
-
+for number in {1979..2016}
+do
+infile="swe_max_${number}.tif"
+outfile="swe_rp_${number}.tif"
 echo "Warping ${infile} using the SRS of ${targetfile} to ${outfile}"
-
 projectionfile=$(mktemp /tmp/tif2targetsrs.XXXXXX)
-
 gdalsrsinfo -o wkt "${targetfile}" > "${projectionfile}"
 gdalwarp -tr .125 -.125 -t_srs "${projectionfile}" "${infile}" "${outfile}"
 rm "${projectionfile}"
-
+done
